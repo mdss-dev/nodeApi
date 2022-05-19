130e950e754e74231397ddddd2974718a4575036
mdss-dev <douglas.ti.dev@gmail.com>
2022-05-19 18:22:23 -0300

    script 'dev' adicionado e rotas simbolicas criadas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ const express = require("express");
 
 const app = express();
 
+app.get("/course", (req, res) => {
+  return res.json(["Course 1", "Course 2", "Course 3"]);
+});
+
+app.post("/course", (req, res) => {
+  return res.json(["Course 1", "Course 2", "Course 3", "Course 4"]);
+});
+
+app.put("/course/:id", (req, res) => {
+  return res.json(["Course 6", "Course 2", "Course 3", "Course 4"]);
+});
+
+app.patch("/course/:id", (req, res) => {
+  return res.json(["Course 6", "Course 7", "Course 3", "Course 4"]);
+});
+
+app.delete("/course/:id", (req, res) => {
+  return res.json(["Course 6", "Course 7", "Course 4"]);
+});
+
 app.listen(3000, function () {
   console.log("Api started");
 });
